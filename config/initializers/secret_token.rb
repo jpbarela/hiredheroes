f217ec857f98577113b503e1b8f9e14d694d82dc
J.P. Barela <barela@jpbarela.com>
2013-06-01 13:00:11 -0600

    Initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hiredheroes::Application.config.secret_token = 'a70cd73fa199126a1d0e108c38db0cb48572cfde696641a151f90b104329c926f907f973b6e3039d8cb6ff6d969d7c8875f46a1408b718471c987daa0cd76116'
